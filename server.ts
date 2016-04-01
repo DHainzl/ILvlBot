20d81f06c799ce73674716cef5a41c4fc022c550
David Hainzl <david@dhainzl.at>
2016-04-02 01:43:45 +0200

    Trying to finally fix azure deployment

--- server.ts
+++ server.ts
@@ -7,19 +7,18 @@ import { BotConnectorBot } from 'botbuilder';
 import { ILvlBot } from './bot/ILvlBot';
 
 let bot = new ILvlBot(
-    new BotConnectorBot(),
+    new BotConnectorBot({
+        appId: process.env.APP_ID || 'YourAppId',
+        appSecret: process.env.APP_SECRET || 'YourAppSecret'
+    }),
     process.env.LUIS_ID,
     process.env.LUIS_KEY,
     process.env.BATTLENET_KEY
 );
     
-let server = restify.createServer();
-server.use(bot.getBot().verifyBotFramework({
-    appId: process.env.APP_ID || 'YourAppId',
-    appSecret: process.env.APP_SECRET || 'YourAppSecret'
-}));
 
-server.post('/v1/messages', bot.getBot().listen());
+let server = restify.createServer();
+server.post('/api/messages', bot.getBot().verifyBotFramework(), bot.getBot().listen());
 server.listen(process.env.port || 3978, function () {
 	console.log('%s listening to %s', server.name, server.url);
 });
